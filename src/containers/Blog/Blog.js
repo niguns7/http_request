74eafd8ba864b12078b4a8f5916e0ac1ca9c4b7d
niguns7 <nirja767@gmail.com>
2022-11-09 15:22:59 +0545

    updated the title and authername

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,19 +6,43 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    //seting the state which we have manipulated after fetching to the api
+    state = {
+        posts: []
+    }
+
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
+        //getting data from our backend
         .then(Response => {
-            console.log(Response)
+            const posts = Response.data.slice(0, 4);
+            const updatedPosts = posts.map(posts => {
+                return {
+                    ...posts,
+                    author: 'Max'
+                //updating the post and setting aurthor name max coz we dont have it on backend
+                }
+            })
+            //rendering to the scereen by updating the state
+            this.setState({posts: updatedPosts});
+            //console.log(Response)
         });
     }
     render () {
+
+        const posts = this.state.posts.map(post => {
+            return <Post 
+            key={post.id }
+            title={post.title}
+            author={post.author}/> 
+            //passing the title to the title and we have diffrent unique key on our api..
+            
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
